316a96edbba31b62107fe1d7ede9d22cf19b7906
Jahdiel C <jcastaneda@belizebank.com>
2026-04-09 16:23:04 -0600

    changed project nomenclature to prepare for template

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
     apps: [{
-        name: 'hold-api',
+        name: 'hono-api',
         script: './dist/index.js',
         instances: 2,
         exec_mode: 'cluster',
-        cwd: '/home/jcastaneda/development/hold-api',
-        out_file: '/home/jcastaneda/.pm2/logs/hold-api-out.log',
-        error_file: '/home/jcastaneda/.pm2/logs/hold-api-error.log',
+        cwd: '/home/jcastaneda/development/hono-api',
+        out_file: '/home/jcastaneda/.pm2/logs/hono-api-out.log',
+        error_file: '/home/jcastaneda/.pm2/logs/hono-api-error.log',
         env_production: {
             NODE_ENV: 'production',
             PORT: 3000,
